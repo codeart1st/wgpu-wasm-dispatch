667a9ecbc1dbfbb485c2363d352a109f3af21b9f
Paul Lange <paul.lange@data-experts.de>
2023-04-20 18:51:00 +0200

    chore: reduce the example

--- tests/test.rs
+++ tests/test.rs
@@ -1,30 +1,16 @@
-#![feature(thread_local)]
-#![allow(clippy::await_holding_refcell_ref)]
-#![cfg(target_arch = "wasm32")]
-
+use wasm_bindgen::prelude::*;
 use wasm_bindgen_test::*;
-
-use std::cell::RefCell;
-use std::sync::Once;
-use wasm_bindgen::JsCast;
-
-pub const CANVAS_SIZE: (u32, u32) = (512, 512);
-
-static INIT: Once = Once::new();
-
-#[thread_local]
-pub static CANVAS: RefCell<Option<web_sys::HtmlCanvasElement>> = RefCell::new(None);
 
 wasm_bindgen_test_configure!(run_in_browser);
 
-pub fn initialize() {
-  INIT.call_once(|| {
+#[wasm_bindgen_test]
+fn empty() {
     let document = web_sys::window().unwrap().document().unwrap();
     let canvas = document.create_element("canvas").unwrap();
     let canvas = canvas
-      .dyn_into::<web_sys::HtmlElement>()
-      .map_err(|_| ())
-      .unwrap();
+        .dyn_into::<web_sys::HtmlElement>()
+        .map_err(|_| ())
+        .unwrap();
 
     let body = document.body().unwrap();
     let style = canvas.style();
@@ -33,25 +19,28 @@ pub fn initialize() {
     style.set_property("position", "absolute").unwrap();
     style.set_property("top", "1em").unwrap();
     style.set_property("right", "1em").unwrap();
+    style.set_property("background", "orange").unwrap();
 
     let canvas: web_sys::HtmlCanvasElement = canvas
-      .dyn_into::<web_sys::HtmlCanvasElement>()
-      .map_err(|_| ())
-      .unwrap();
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|_| ())
+        .unwrap();
 
-    let (width, height) = CANVAS_SIZE;
-    canvas.set_width(width);
-    canvas.set_height(height);
+    canvas.set_width(512);
+    canvas.set_height(512);
 
-    *CANVAS.borrow_mut() = Some(canvas);
-  });
-}
+    console_error_panic_hook::set_once();
 
-#[wasm_bindgen_test]
-async fn empty() {
-  initialize();
+    let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+        backends: wgpu::util::backend_bits_from_env().unwrap_or(wgpu::Backends::all()),
+        dx12_shader_compiler: wgpu::util::dx12_shader_compiler_from_env().unwrap_or_default(),
+    });
 
-  let canvas_ref = CANVAS.borrow();
-  let canvas = canvas_ref.as_ref().unwrap();
-  wgpu_wasm_dispatch::wasm::start_with_canvas(canvas).await;
+    // fails: assertion failed: data.is::<T>()
+    let _ = match instance.create_surface_from_canvas(&canvas) {
+        Ok(surface) => surface,
+        Err(err) => {
+            panic!("{}", err.to_string())
+        }
+    };
 }
